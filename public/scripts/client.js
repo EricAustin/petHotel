700345d3f66d998449c629e164cb3afae7f587e3
Eric Austin <ewaustin@gmail.com>
2017-08-18 13:39:10 -0500

    dynamic dropdown started

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,12 +6,22 @@ app.controller('PetHotelController', ['$http', function ($http) {
     console.log('Pet Hotel Controller loaded');
     var self = this;
 
-    self.names = ["Emil", "Tobias", "Linus"];
+    self.names = [];
+
+    self.getOwners = function () {
+        $http({
+            method: 'GET',
+            url: '/owners'
+        }).then(function(response) {
+            console.log('response.data');
+            self.names = response.data;
+        })  //then end
+    } // getOwners end
 
 
 
 
-}])
+}]) // controller end
 
 // app.controller('ownerDropdown', function ($scope) {
 //     $scope.names = ["Emil", "Tobias", "Linus"];
