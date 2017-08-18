4322d3204e7d9fe44497a872120136f582b30b46
Eric Austin <ewaustin@gmail.com>
2017-08-18 08:44:06 -0500

    server updated

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var message = require('.routes/message');
+// var ????????) = require('.routes/????????)');
 
 var app = express();
 
@@ -8,6 +8,11 @@ var port = 5000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use('/????????)', ????????)
+
+
+
 
 app.listen(port, function(){
     console.log('listening on port', port);
