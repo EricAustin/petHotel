dda11824eb366892b60aa3415086c97b789a6d71
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-18 09:26:34 -0500

    GET and POST on owner.js added. pet.js started

---
+++ server/routes/owner.js
@@ -0,0 +1,50 @@
+// owner.js
+
+var express = require('express');
+var router = express.Router();
+var pool = require('../modules/pool');
+
+router.post('/', function (req, res) {
+    console.log('owner post');
+    // Add an INSERT query
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // when connecting to database failed
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // when connecting to database worked!
+            client.query('INSERT INTO owners (first_name, last_name) VALUES ($1, $2);', [req.body.first_name, req.body.last_name], function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
+                }
+            });
+        }
+    });
+});
+
+router.get('/', function (req, res) {
+    console.log('owner get');
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            client.query('SELECT * FROM owners;', function (errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    console.log('Error making database query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
